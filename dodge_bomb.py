ae89deb801e8c91d8a269599a62ebf8d1efe0955
arata <c0a24226ed@edu,teu.ac.jp>
2025-04-22 17:06:02 +0900

    演習１～２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,8 +1,8 @@
 import os
 import random
 import sys
+import time
 import pygame as pg
-
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
@@ -18,30 +18,26 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """ 
     引数：こうかとんRectまたは爆弾Rect
     戻り値：判定結果タプル（横，縦）
-    画面内ならTrue 画面外ならFalse
+    画面内ならTrue，画面外ならFalse
     """
-    yoko, tate = True, True  # 横，縦方向の変数
-    # 横方向
+    yoko, tate = True, True  # 横，縦方向用の変数
+    # 横方向判定
     if rct.left < 0 or WIDTH < rct.right:  # 画面外だったら
         yoko = False
-    # 縦方向
-    if rct.top < 0 or HEIGHT < rct.bottom: # 画面外だったら
+    # 縦方向判定
+    if rct.top < 0 or HEIGHT < rct.bottom:  # 画面外だったら
         tate = False
     return yoko, tate
-
-
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    
     # こうかとん初期化
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    
     # 爆弾初期化
     bb_img = pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
@@ -53,19 +49,60 @@ def main():
 
     clock = pg.time.Clock()
     tmr = 0
+    
+    kurohaikei_img = pg.Surface((1100, 650))
+    pg.draw.rect(kurohaikei_img, (0, 0, 0), (0, 0, 1100, 650))
+    kurohaikei_rct = kurohaikei_img.get_rect()
+    kurohaikei_rct.center = 550, 325
+    kurohaikei_img.set_alpha(200)
+    
+    font = pg.font.Font(None, 80)
+    txt = font.render("Game Over", True, (255, 255, 255))
+    
+    vx, vy = +5, +5
+    clock = pg.time.Clock()
+    tmr = 0
+    
+    cry_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+
+    def gameover(screan: pg.Surface) -> None:
+        screan.blit(kurohaikei_img, kurohaikei_rct)
+        screan.blit(txt, [400, 300])
+        screan.blit(cry_img, [730, 300])
+        screan.blit(cry_img, [330, 300])
+        pg.display.update()
+        time.sleep(5)
+        
+    def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    
+        bb_imgs = []
+        bb_accs = [i for i in range(1, 11)]
+        for r in range(1, 11):
+            img = pg.Surface((20 * r, 20 * r))
+            pg.draw.circle(img, (255, 0, 0), (10 * r, 10 * r), 10 * r)
+            img.set_colorkey((0, 0, 0))
+            bb_imgs.append(img)
+        return bb_imgs, bb_accs
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        # こうかとんRectと爆弾Rectが重なっていたら
+        if kk_rct.colliderect(bb_rct): 
+            gameover(screen)
+            print("Game Over")
+            return
+
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
 
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]  # 左右方向
-                sum_mv[1] += mv[1]  # 上下方向
+                sum_mv[0] += mv[0]  # 左右
+                sum_mv[1] += mv[1]  # 上下
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -75,10 +112,10 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True): # 画面外だったら
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) # 画面内に戻す
+        if check_bound(kk_rct) != (True, True):  # 画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])  # 画面内に戻す
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy)  # 爆弾の移動
+        bb_rct.move_ip(vx, vy)  
         yoko, tate = check_bound(bb_rct)
         if not yoko:  # 左右どちらかにはみ出ていたら
             vx *= -1
@@ -88,7 +125,15 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
+        
+        bb_imgs, bb_accs = init_bb_imgs()
 
+        #加速、巨大化
+        idx = min(tmr // 500, 9)
+        bb_img = bb_imgs[idx]
+        avx = vx * bb_accs[idx]
+        avy = vy * bb_accs[idx]
+        bb_rct.move_ip(avx, avy)
 
 if __name__ == "__main__":
     pg.init()
